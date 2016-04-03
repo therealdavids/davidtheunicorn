f6b865f61920a9f4d17eaeeb83eab05492e4a9a8
David Nuon <david@davidnuon.com>
2016-04-02 20:28:21 -0700

    Added two guys moving around on screen

--- src/objects/Logo.js
+++ src/objects/Logo.js
@@ -11,12 +11,13 @@ export default class Logo extends Phaser.Sprite {
 
   constructor (game, x, y) {
     super(game, x, y, 'phaser');
-
+    this.width = 30;
+    this.height = 30;
     this.anchor.set(0.5);
   }
 
   update () {
-    this.angle += 0.1;
+    this.angle += 0.1;    
   }
 
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -13,7 +13,63 @@ export default class Game extends Phaser.State {
     // TODO: Replace this with really cool game code here :)
 
     const { centerX: x, centerY: y } = this.world;
-    this.add.existing(new Logo(this.game, x, y));
+    var logo_p1 = new Logo(this.game, x - 50, y - 50);
+    var logo_p2 = new Logo(this.game, x, y);
+    this.logo_p1 = logo_p1;
+    this.logo_p2 = logo_p2;
+    this.add.existing(logo_p1);
+    this.add.existing(logo_p2);
   }
+
+  update() {
+
+
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.W)) {
+	    	console.log('UP');
+	    	this.logo_p2.y -= 10;
+	    }  	
+
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.S)) {
+	    	console.log('DOWN');
+	    	this.logo_p2.y += 10;
+	    }  		    
+
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.A)) {
+	    	console.log('LEFT');
+	    	this.logo_p2.x -= 10;
+	    }
+
+
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.D)) {
+			console.log('xxxxRIGHT');
+			this.logo_p2.x += 10;
+	    }
+
+
+
+
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+	    	console.log('UP');
+	    	this.logo_p1.y -= 10;
+	    }  	
+
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+	    	console.log('DOWN');
+	    	this.logo_p1.y += 10;
+	    }  		    
+
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+	    	console.log('LEFT');
+	    	this.logo_p1.x -= 10;
+	    }
+
+
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+			console.log('xxxxRIGHT');
+			this.logo_p1.x += 10;
+	    }
+
+  }
+
 
 }
